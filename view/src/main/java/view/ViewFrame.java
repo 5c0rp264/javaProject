aff1400ba0306f3cf964683ca3cc615d5644b7ca
hacker264 <Quentin.aoustin@live.fr>
2019-05-22 16:39:00 +0200

    Displaying weirdly a single block
    Trying to implement database with level

--- view/src/main/java/view/ViewFrame.java
+++ view/src/main/java/view/ViewFrame.java
@@ -133,6 +133,7 @@ class ViewFrame extends JFrame implements KeyListener {
 		this.setContentPane(new ViewPanel(this));
 		this.setSize(1000 + this.getInsets().left + this.getInsets().right, 1000 + this.getInsets().top + this.getInsets().bottom);
 		this.setLocationRelativeTo(null);
+		
 	}
 
 	/**
